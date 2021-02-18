41f9b1d43a63aaa8b425b37b89cde5ebcbd058d5
Domenico <domenicograsso@gmail.com>
2021-02-18 15:36:54 +0100

    fine menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 new Vue({
   el: "#app",
   data: {
-    prova: 2,
+    activeLinkMenu: 0,
+    menuLink: [
+      {
+        name: "home",
+        url: "#home",
+      },
+      {
+        name: "about",
+        url: "#",
+      },
+      {
+        name: "service",
+        url: "#",
+      },
+      {
+        name: "work",
+        url: "#",
+      },
+      {
+        name: "article",
+        url: "#",
+      },
+      {
+        name: "get quote",
+        url: "#",
+      },
+    ],
   },
 });
 Vue.config.devtools = true;
